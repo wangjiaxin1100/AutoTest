20a81430f08acb67510f955ec4bcbe941554ed46
wangjiaxin1100 <362334622@qq.com>
2019-01-24 15:04:39 +0800

    第九章内容

--- Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -1,4 +1,49 @@
 package com.course.httpclient.cookies;
 
+import org.apache.http.HttpResponse;
+import org.apache.http.client.CookieStore;
+import org.apache.http.client.HttpClient;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.cookie.Cookie;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.util.EntityUtils;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+import java.util.ResourceBundle;
+
 public class MyCookiesForGet {
+    private String url;
+    private ResourceBundle bundle;
+    @BeforeTest
+    public void beforeTest(){
+        bundle = ResourceBundle.getBundle("application", Locale.CHINA);
+        url = bundle.getString("test.url");
+    }
+    @Test
+    public void testGetCookies() throws IOException {
+        String result;
+        ///从配置文件中拼接测试URL
+        String uri = bundle.getString("getCookies.uri");
+        String testUrl = this.url+uri;
+        HttpGet get = new HttpGet(testUrl);
+        HttpClient client = new DefaultHttpClient();
+        HttpResponse response = client.execute(get);
+        result = EntityUtils.toString(response.getEntity(),"utf-8");
+        System.out.println(result);
+        ///获取Cookies信息
+        CookieStore store = ((DefaultHttpClient) client).getCookieStore();
+        List<Cookie> cookieList =store.getCookies();
+        for (Cookie cookie : cookieList){
+            String name = cookie.getName();
+            String value= cookie.getValue();
+            System.out.println("cookies name=" + name + ";cookie value="+value);
+        }
+
+    }
+
 }
+git
