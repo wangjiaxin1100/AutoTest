0a35c4c9dacbc815434ea28c43f67704c9396009
wangjiaxin1100 <362334622@qq.com>
2019-01-24 16:30:56 +0800

    第九章Get方法完成

--- Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
+++ Chapter9/src/main/java/com/course/httpclient/cookies/MyCookiesForGet.java
@@ -18,6 +18,8 @@ import java.util.ResourceBundle;
 public class MyCookiesForGet {
     private String url;
     private ResourceBundle bundle;
+    //用来存储Cookies信息的变量
+    private CookieStore store;
     @BeforeTest
     public void beforeTest(){
         bundle = ResourceBundle.getBundle("application", Locale.CHINA);
@@ -29,13 +31,14 @@ public class MyCookiesForGet {
         ///从配置文件中拼接测试URL
         String uri = bundle.getString("getCookies.uri");
         String testUrl = this.url+uri;
+        //测试逻辑代码
         HttpGet get = new HttpGet(testUrl);
         HttpClient client = new DefaultHttpClient();
         HttpResponse response = client.execute(get);
         result = EntityUtils.toString(response.getEntity(),"utf-8");
         System.out.println(result);
         ///获取Cookies信息
-        CookieStore store = ((DefaultHttpClient) client).getCookieStore();
+        this.store = ((DefaultHttpClient) client).getCookieStore();
         List<Cookie> cookieList =store.getCookies();
         for (Cookie cookie : cookieList){
             String name = cookie.getName();
@@ -44,6 +47,26 @@ public class MyCookiesForGet {
         }
 
     }
+    @Test(dependsOnMethods = {"testGetCookies"})
+    public void testGetWithCookies() throws IOException{
+        String uri = bundle.getString("test.get.with.cookies");
+        String testUrl = this.url+uri;
+        HttpGet get = new HttpGet(testUrl);
+        DefaultHttpClient client = new DefaultHttpClient();
+
+        //设置Cookies信息
+        client.setCookieStore(this.store);
+        HttpResponse response = client.execute(get);
+
+        //获取响应状态码
+        int StatusCode =response.getStatusLine().getStatusCode();
+        System.out.println("statuscode = "+ StatusCode);
+
+        if (StatusCode == 200){
+            String result = EntityUtils.toString(response.getEntity(),"utf-8");
+            System.out.println(result);
+        }
+
+    }
 
 }
-git
