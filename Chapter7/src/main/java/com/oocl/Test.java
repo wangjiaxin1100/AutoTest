68d4d1ca6920b409979ff5d32228ef57f7242590
wangjiaxin1100 <362334622@qq.com>
2019-04-28 10:57:01 +0800

    更新第七章

--- Chapter7/src/main/java/com/oocl/Test.java
+++ Chapter7/src/main/java/com/oocl/Test.java
@@ -1,5 +1,14 @@
 package com.oocl;
 
-public class Test {
+import com.github.dreamhead.moco.HttpServer;
+import static com.github.dreamhead.moco.Moco.*;
 
-}
+
+public class Test {
+@org.junit.Test
+public void  HttpsServer(){
+        HttpServer server = httpServer(8899);
+        server.request(by(uri("/foo"))).response("bar");
+        System.out.println("服务起来了");
+            }
+        };
